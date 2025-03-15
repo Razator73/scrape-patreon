55c32312aace2d3f5c908b4578e588ce38f04d46
Ryan Scott <ryan.t.scott73@gmail.com>
2025-03-15 15:26:58 -0600

    Initial commit

---
+++ omar_downloads.py
@@ -0,0 +1,74 @@
+import os
+import time
+from pathlib import Path
+
+import gdown
+from dotenv import load_dotenv
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+def fetch_collection_links(wd, count, name, url):
+    download_folder = Path.home() / 'Downloads' / name
+    download_folder.mkdir(exist_ok=True)
+    print(f'Getting posts for {name}')
+    if len([f for f in download_folder.glob('*')]) == count:
+        print('\tNumber of files equal the post count. Skipping')
+        return []
+    wd.get(url)
+    time.sleep(3)
+    while load_more := [e for e in wd.find_elements(By.XPATH, "//div[contains(@class, 'sc-eCImPb cXvBBE')]")
+                        if e.text == 'Load more']:
+        load_more[0].click()
+        time.sleep(5)
+    post_divs = wd.find_elements(By.XPATH, "//div[contains(@data-tag, 'post-card')]")[::-1]
+    print(f'\tFound {len(post_divs)}')
+    col_posts = []
+    for i, col_post in enumerate(post_divs):
+        file_name, download_link = col_post.text.split('\n')[:2]
+        file_name = f'{i + 1:03} {file_name}'
+        col_posts.append((download_folder / file_name, download_link))
+    return col_posts
+    #     if not (download_file := download_folder / post_name).exists():
+    #         # print(f'Download to {download_file}')
+    #         gdown.download(post_link, download_file, quiet=False)
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    download_cols = ['Dragonball', 'Breaking Bad', 'FMAB']
+
+    with webdriver.Chrome() as driver:
+        driver.get('https://www.patreon.com/login')
+        email_element = driver.find_element(By.NAME, 'email')
+        email_element.send_keys(os.environ['PATREON_USERNAME'])
+        email_element.submit()
+        time.sleep(1)
+        driver.find_element(By.NAME, 'current-password').send_keys(os.environ['PATREON_PASSWORD'])
+        email_element.submit()
+        time.sleep(2)
+        driver.get('https://www.patreon.com/c/omariorpg/collections')
+        time.sleep(10)
+        collections = driver.find_elements(By.XPATH, "//div[contains(@class, 'sc-ieecCq hlVSeb')]")
+        collection_links = {}
+        for col in collections:
+            col_link = col.find_element(By.TAG_NAME, 'a')
+            col_count, col_name = col.text.split('\n')
+            collection_links[col_name] = {'name': col_name, 'count': int(col_count),
+                                          'url': col_link.get_property('href')}
+        for col in download_cols:
+            if col not in collection_links:
+                print(f"Couldn't find the collection: {col}")
+                continue
+            collection_links[col]['posts'] = fetch_collection_links(driver, **collection_links[col])
+    all_posts = {col['name']: col['posts'] for col in collection_links.values() if 'posts' in col}
+    for col, posts in all_posts.items():
+        print(f'Downloading missing posts for {col}...')
+        for post in posts:
+            if post[0].exists():
+                print(f'{post[0].name} exists')
+                continue
+            gdrive_id = post[1].split('/')[-2]
+            down_url = f'https://drive.google.com/uc?id={gdrive_id}'
+            # print(f'\tDownloading {post[0].name}')
+            gdown.download(down_url, str(post[0]), quiet=False)
